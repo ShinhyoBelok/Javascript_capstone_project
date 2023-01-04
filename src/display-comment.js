ac9a4797564b4837ff3507e471d26a38812e4f6b
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-04 10:46:50 -0500

    Fix for for forEach and using destructuring in name

--- src/display-comment.js
+++ src/display-comment.js
@@ -2,10 +2,9 @@
 import { showPopup, popup } from './popup-comment.js';
 
 /// / Comment button
-const commentButtons = document.getElementsByClassName('comments');
-
+const commentButtons = document.querySelectorAll('.comments');
+console.log (commentButtons);
 const buttonPressed = (e) => {
-  // console.log(e.target.id); // Get ID of Clicked Element
   const sid = e.target.id.substring(4);
   // console.log(e.target.id, sid); // Get ID of Clicked Element
   const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${sid}`;
@@ -13,7 +12,7 @@ const buttonPressed = (e) => {
     .then((response) => response.json())
     .then((json) => {
       const pokemon = json;
-      const name = pokemon.name;
+      const { name } = pokemon;
       // const image = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`;
       // const image = pokemon.sprites.front_default;
       const image = pokemon.sprites.other.dream_world.front_default;
@@ -30,8 +29,8 @@ const buttonPressed = (e) => {
     });
 };
 
-for (const button of commentButtons) {
+commentButtons.forEach((button) => {
   button.addEventListener('click', buttonPressed);
-}
+});
 
 export default buttonPressed;
