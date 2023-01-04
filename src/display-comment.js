f45a1065e6113630a9b7748305c5a51d9b014063
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-04 04:26:40 -0500

    Fixing linters errors

--- src/display-comment.js
+++ src/display-comment.js
@@ -1,4 +1,5 @@
-import { showPopup, getComments, addLike, popup, getCommentID } from './popup-comment.js';
+// import { showPopup, getComments, addLike, popup, getCommentID } from './popup-comment.js';
+import { showPopup, popup} from './popup-comment.js';
 
 /// / Comment button
 const commentButtons = document.getElementsByClassName('comments');
@@ -15,21 +16,21 @@ const buttonPressed = (e) => {
       popup.classList.toggle('display');
       const name = pokemon.name;
       // const image = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`;
-      //const image = pokemon.sprites.front_default;
+      // const image = pokemon.sprites.front_default;
       const image = pokemon.sprites.other.dream_world.front_default;
-      //const image = pokemon.sprites.other.official-artwork.front_default;
+      // const image = pokemon.sprites.other.official-artwork.front_default;
       const info = `
       <p class="popup-detail-item">Height : ${pokemon.height}</p>
       <p class="popup-detail-item">Weight : ${pokemon.weight}</p>
       <p class="popup-detail-item">Base Experience : ${pokemon.base_experience}</p>
       <p class="popup-detail-item">Moves : ${pokemon.moves.length}</p> `;
       showPopup(name, image, info);
-      //getComments(pokemon.id);
-      //getCommentID(pokemon.id);
+      // getComments(pokemon.id);
+      // getCommentID(pokemon.id);
     });
 };
 
-for (let button of commentButtons) {
+for (const button of commentButtons) {
   button.addEventListener('click', buttonPressed);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
 import logo from './img/logo.png';
 import './display-comment.js'
-const imageLogo = document.querySelector('.logo').src=logo;
+
+document.querySelector('.logo').src = logo;
