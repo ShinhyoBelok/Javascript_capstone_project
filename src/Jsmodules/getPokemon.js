e9dc3af7fb9297ca97cb5fe903e5c7758c8cd4ec
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-03 23:36:06 -0400

    Generate the pokemon list dynamically from the API

--- src/Jsmodules/getPokemon.js
+++ src/Jsmodules/getPokemon.js
@@ -1,9 +1,19 @@
-const pokemonContainer = document.querySelector('.pokemon-container');
+import showPokemon from "./displayPokemon.js";
 
-const fetchPokemon = async () => {
-  await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
-  .then(res => res.json())
-  .then(data => console.log(data))
+const getAllPokemons = (num = 100) => {
+  const promises = [];
+  for (let id = 1; id < num; id++) {
+    const pokemonAPI = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    promises.push(fetch(pokemonAPI).then(res => res.json()));
+  }
+  Promise.all(promises).then(res => {
+    const pokemons = res.map(data => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites['front_default']
+    }));
+    showPokemon(pokemons);
+  });
 }
 
-export default fetchPokemon;
+export default getAllPokemons;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
 import logo from './img/logo.png';
+import getAllPokemons from './Jsmodules/getPokemon';
 
-const icon = document.querySelector('.logo').src=logo;
-
+document.querySelector('.logo').src=logo;
+getAllPokemons();
 
 
