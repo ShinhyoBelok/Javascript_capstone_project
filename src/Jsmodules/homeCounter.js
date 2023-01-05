f35d5b9e082c87f20fca0bdd2de4e4e6c5fd22d8
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-05 17:20:54 -0400

    Add jest testing for the home-counter

--- src/Jsmodules/homeCounter.js
+++ src/Jsmodules/homeCounter.js
@@ -2,6 +2,7 @@ const pokemonCounter = () => {
   const pokemonCard = document.querySelectorAll('.pokemonCard');
   const homeCounter =  document.querySelector('.home-counter');
   homeCounter.innerHTML = `&nbsp;(${pokemonCard.length})`;
+  return pokemonCard.length;
 }
 
 export default pokemonCounter;
