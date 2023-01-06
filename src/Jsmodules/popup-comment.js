85b81835c9abff970a9a569219064d221eb5e686
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-05 23:58:34 -0500

    Use ut-counter-check for comments counter

--- src/Jsmodules/popup-comment.js
+++ src/Jsmodules/popup-comment.js
@@ -1,5 +1,7 @@
 import './popup-comment.css';
 import pokemon3 from '../img/pokemon3.png';
+import utCounterCheck from './ut-counter-and-check.js';
+
 //  popup
 export const popup = document.createElement('div');
 popup.id = 'popup';
@@ -124,7 +126,7 @@ export const getComments = async (itemId) => {
       popupCommentHeader.innerHTML = 'Comments (0)';
       popupCommentList.innerHTML = '';
     } else {
-      popupCommentHeader.innerHTML = `Comments (${json.length})`;
+      // popupCommentHeader.innerHTML = `Comments (${json.length})`;
       popupCommentList.innerHTML = '';
       json.forEach((element) => {
         const newComment = document.createElement('li');
@@ -137,6 +139,8 @@ export const getComments = async (itemId) => {
       });
     }
   }).catch((e) => e);
+  const qtyComments = utCounterCheck('popup-comment-item');
+  popupCommentHeader.innerHTML = `Comments (${qtyComments})`;
 };
 
 //  add new comment
